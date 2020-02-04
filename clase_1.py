35fe2e982f0f444cc5265ad66dfcc9be668f286f
scuccu <36194059+scuccu@users.noreply.github.com>
2020-02-03 21:48:02 -0300

    Add files via upload

---
+++ clase_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#print("Hola mundo")
+#print("La suma de 2+3 es: ")
+#suma=(2+3)
+#print(suma)
+
+#saludo=("Hola ")
+#nombre=("Sofìa")
+
+#salida=saludo+nombre #sirve para concatenar el contenido de las variables
+#print(salida)
+
+iva=0.21
+precio_producto = 150
+precio_iva= precio_producto * iva
+
+print("El precio del iva es: ", precio_iva,"$")
+print("El precio final es: ",precio_producto+precio_iva,"$")
